f25562d049cbf37b03d245d9b34466f87134d93a
vincent <jonanv@uw.edu>
2016-04-19 10:26:22 -0700

    added bluetooth

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -20,6 +20,8 @@ import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -92,12 +94,12 @@ public class BluetoothChatFragment extends Fragment {
         setHasOptionsMenu(true);
 
         // TODO: Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null) {
+            Toast.makeText(getActivity().getApplicationContext(), "no bluetooth blabla", Toast.LENGTH_LONG);
+            getActivity().finish();
+        }
 
-        /**
-         *  TODO: If the adapter is null, then Bluetooth is not supported.
-         *  TODO: Let us know via Toast Bluetooth is not available.
-         *  TODO: Then finish() the fragment's attached Activity.
-         */
     }
 
 
@@ -109,6 +111,10 @@ public class BluetoothChatFragment extends Fragment {
             // setupChat() will then be called during onActivityResult
 
             if(!mBluetoothAdapter.isEnabled()) {
+
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+
 
                 /**
                  * TODO: If adapter is NOT enabled, send an Implicit Intent with the action ACTION_REQUEST_ENABLE (send for a result)
@@ -199,6 +205,10 @@ public class BluetoothChatFragment extends Fragment {
      */
     private void ensureDiscoverable() {
 
+        if(mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        }
         /**
          * TODO: Check if the adapter is in SCAN_MODE_CONNECTABLE_DISCOVERBLE mode
          * TODO: If not, send an Implicit Intent for the ACTION_REQUEST_DISCOVERABLE action (with an extra for the EXTRA_DISCOVERABLE_DURATION)
